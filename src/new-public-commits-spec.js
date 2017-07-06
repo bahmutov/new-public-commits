524dd8054c6edd2c369ff055c461d1df367335d6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-06 18:15:53 -0400

    initial

---
+++ src/new-public-commits-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const newPublicCommits = require('.')
+
+describe('new-public-commits', () => {
+  it('write this test', () => {
+    console.assert(newPublicCommits, 'should export something')
+  })
+})
